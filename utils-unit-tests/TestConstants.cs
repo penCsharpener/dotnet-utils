d0ee52e9c082b35ffa4d2ed27b5c6eb54e878bf5
penCsharpener <matthiasmueller@protonmail.com>
2019-04-09 22:42:41 +0200

    add TestConstants to unit tests

---
+++ utils-unit-tests/TestConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace penCsharpener.DotnetUtils.NUnit {
+    public static class TestConstants {
+        public const string partsMozillaPath = @"C:\Users\|\AppData\Local\Mozilla\Firefox\Profiles\|.default\cache2\entries";
+        public const string partsChromePath = @"C:\Users\|\AppData\Local\Google\Chrome\User Data\Default\Code Cache\js";
+        public const string partsChromePath2 = @"C:\Users\|\AppData\Local\Google\Chrome\User Data\Default\Cache";
+        public const string expectFalseParts = @"D:\Software\|\Firefox\Profiles\|\cache2\entries";
+        public const string testMozillaPath = @"C:\Users\MyUsername\AppData\Roaming\Mozilla\Firefox\Profiles\clsow3acld9s.default\cache2\entries";
+    }
+}

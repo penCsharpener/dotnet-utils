8a82333cd58f120c8e089a55fa174be58407cd1b
penCsharpener <matthiasmueller@protonmail.com>
2019-04-10 23:24:03 +0200

    add various FileInfo to Bytes or hash extensions

---
+++ DotnetUtils/ByteExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace penCsharpener.DotnetUtils {
+    public static class ByteExtensions {
+
+        public static byte[] ToUTF8Bytes(this string str) {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        public static byte[] ToBytes(this string str, Encoding encoding) {
+            return encoding.GetBytes(str);
+        }
+
+        public static string ToString(this byte[] bytes, Encoding encoding) {
+            return encoding.GetString(bytes, 0, bytes.Length);
+        }
+
+        public static byte[] ToBytes(this FileInfo fileInfo) {
+            return File.ReadAllBytes(fileInfo.FullName);
+        }
+
+        public static string ToBase64(this FileInfo fileInfo) {
+            return Convert.ToBase64String(File.ReadAllBytes(fileInfo.FullName));
+        }
+
+        public static string ToHex(this byte[] bytes) {
+            return string.Concat(bytes.Select(x => x.ToString("X2"))).ToLower();
+        }
+
+        public static string ToBase64(this byte[] bytes) {
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
---
+++ DotnetUtils/HashExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace penCsharpener.DotnetUtils {
+    public static class HashExtensions {
+
+        public static string ToMD5(this FileInfo fileInfo) {
+            using (var sha1 = new MD5CryptoServiceProvider()) {
+                var byteHash = sha1.ComputeHash(fileInfo.ToBytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToSha1(this FileInfo fileInfo) {
+            using (var sha1 = new SHA1Managed()) {
+                var byteHash = sha1.ComputeHash(fileInfo.ToBytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToSha256(this FileInfo fileInfo) {
+            using (var sha1 = new SHA256Managed()) {
+                var byteHash = sha1.ComputeHash(fileInfo.ToBytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToSha512(this FileInfo fileInfo) {
+            using (var sha1 = new SHA512Managed()) {
+                var byteHash = sha1.ComputeHash(fileInfo.ToBytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToMD5(this string str) {
+            using (var sha1 = new MD5CryptoServiceProvider()) {
+                var byteHash = sha1.ComputeHash(str.ToUTF8Bytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToSha1(this string str) {
+            using (var sha1 = new SHA1Managed()) {
+                var byteHash = sha1.ComputeHash(str.ToUTF8Bytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToSha256(this string str) {
+            using (var sha1 = new SHA256Managed()) {
+                var byteHash = sha1.ComputeHash(str.ToUTF8Bytes());
+                return byteHash.ToHex();
+            }
+        }
+
+        public static string ToSha512(this string str) {
+            using (var sha1 = new SHA512Managed()) {
+                var byteHash = sha1.ComputeHash(str.ToUTF8Bytes());
+                return byteHash.ToHex();
+            }
+        }
+    }
+}
